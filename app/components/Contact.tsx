9b462e14fd28de336e57cc5e80473a545ac29c7c
Lizett Enriquez <karidavila0911@gmail.com>
2026-01-28 14:34:26 -0500

    Adding some styles.

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -8,11 +8,11 @@ const Contact = () => {
     ScrollObserver({ refElement: sectionRef, name: 'link-contact' })
     
     return (
-        <section ref={sectionRef} id="contact" className="py-8 md:py-24 px-2" >
-            <div className="max-w-2xl mx-auto text-center">
+        <section ref={sectionRef} id="contact" className="mt-16 mx-4 md:mx-0" >
+            <div className="max-w-2xl mx-auto text-center bg-gray-700/30 rounded-md p-4">
                 <h2 className="text-3xl font-bold tracking-tight pb-8 text-(--color-title)">Get In Touch</h2>
                 <p className="text-gray-600 dark:text-gray-400">I'm currently open to new opportunities. Feel free to send me a message!</p>
-                <p className="text-gray-600 dark:text-gray-400 mb-8 ">If you want to contact me, please, send me an email to: <strong className="font-bold">lizett.enriquez0911@outlook.com</strong> </p>
+                <p className="text-gray-600 dark:text-gray-400 mb-8 ">If you want to contact me, please, send me an email to: <strong className="font-bold text-teal-500/60">lizett.enriquez0911@outlook.com</strong> </p>
             </div>
         </section>
     )
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -21,7 +21,7 @@ const Projects: FC<ProjectsProps> = ({ projectTitle, projectDescription, project
 
 
     return (
-        <section ref={sectionRef} id="projects" className="py-16 md:pt-24">
+        <section  ref={sectionRef} id="projects" className="pt-20">
             <h2 className="text-3xl font-bold tracking-tight pb-8 text-center text-(--color-title)">My Projects</h2>
             <div className="space-y-12">
                 <div className="bg-[#2D3748] m-6 rounded-xl overflow-hidden shadow-lg transition-shadow hover:shadow-2xl">
@@ -45,10 +45,10 @@ const Projects: FC<ProjectsProps> = ({ projectTitle, projectDescription, project
                                 ))}
                             </div>
                             <div className="flex items-center gap-4 mt-auto">
-                                <Link className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-10 px-4 bg-teal-500 text-white text-sm font-medium leading-normal hover:opacity-90 transition-opacity" href={liveDemoLink} target="_blank" rel="noopener noreferrer    ">
+                                <Link className="shadow-sm shadow-teal-500/40 flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-10 px-4 bg-teal-500 text-white text-sm font-medium leading-normal hover:opacity-90 transition-opacity" href={liveDemoLink} target="_blank" rel="noopener noreferrer    ">
                                     <span className="truncate">Live Demo</span>
                                 </Link>
-                                <Link className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-10 px-4 border border-gray-300 dark:border-gray-600 text-sm font-medium leading-normal hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors" href={codeLink} target="_blank" rel="noopener noreferrer">
+                                <Link className="shadow-sm shadow-gray-500/40 flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-10 px-4 border border-gray-300 dark:border-gray-600 text-sm font-medium leading-normal hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors" href={codeLink} target="_blank" rel="noopener noreferrer">
                                     <span className="truncate text-(--color-title)">View Code</span>
                                 </Link>
                             </div>
